2f5ed664eff20a489a964daa52903881701f011e
BarniBl <veinskiyva@mail.ru>
2019-09-04 00:36:59 +0300

    new update 1

--- source/plain.js
+++ source/plain.js
@@ -1,23 +1,13 @@
 'use strict';
 
-function reducer (accumulator, currentValue){
-	if( Array.isArray(currentValue)){
-		if(currentValue.length === 0){
-			return accumulator;
-		}
-		let tempArray = plain(currentValue);
-		for (let subElement of tempArray){
-			accumulator.push(subElement);	
-		}
-	}
-	else {
-		accumulator.push(currentValue);	
-	}
-	return accumulator;
-}
-
 function plain (array){
-	let newArray = [];
-	newArray = array.reduce(reducer, newArray);
-	return newArray;
+	return array.reduce((accumulator, currentValue)=>{
+	if( Array.isArray(currentValue)){
+		const tempArray = plain(currentValue);
+		accumulator = accumulator.concat(tempArray);
+		return accumulator;
+	}
+	accumulator.push(currentValue);	
+	return accumulator;
+	}, []);
 }
