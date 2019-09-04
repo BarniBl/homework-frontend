5f8bf4925626f0f8b40815c50e8c645de1f4d309
BarniBl <veinskiyva@mail.ru>
2019-09-04 13:14:26 +0300

    upd 3

--- source/plain.js
+++ source/plain.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function plain (array){
-	return array.reduce((accumulator, currentValue)=>{
-	if( Array.isArray(currentValue)){
-		const tempArray = plain(currentValue);
-		return [...accumulator,...tempArray];
-	}
-	return [...accumulator, currentValue];
+	return array.reduce((accumulator, currentValue) => {
+		if(Array.isArray(currentValue)){
+			const tempArray = plain(currentValue);
+			return [...accumulator, ...tempArray];
+		}
+		return [...accumulator, currentValue];
 	}, []);
 }
